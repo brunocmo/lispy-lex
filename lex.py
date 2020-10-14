8a402adabfee1a61d8b630235b49e10bae2b5409
Bruno Nunes <brunocmo.nunes@gmail.com>
2020-10-14 20:38:43 -0300

    resposta final

--- lex.py
+++ lex.py
@@ -13,4 +13,35 @@ def lex(code: str) -> Iterable[Token]:
     da string de código fornecida.
     """
 
-    return [Token('INVALIDA', 'valor inválido')]
+    tokens = {
+        'QUOTE': r'\'',
+        'STRING': r'\".*\"',
+        'LPAR': r'\(',
+        'RPAR': r'\)',
+        'NUMBER': r'[+|-]?\d+(?:\.\d+)?',
+        'NAME': r'([a-zA-Z_%\+\-]|\.\.\.)[a-zA-Z_0-9\-\>\?\!]*',
+        'CHAR': r'#\\[a-zA-Z]*',
+        'BOOL':  r'#[t|f]',
+    }
+
+    token = ''
+    
+    code = re.sub(r';;.*', "", code)
+
+    for key, value in tokens.items():   
+
+        if re.match('n+', key):
+            token += f'(?:{value})|'
+        else:
+            token += f'(?P<{key}>{value})|'
+
+    token = re.compile(token)
+
+    lexfinal = [ ]
+
+    for each in token.finditer(code):
+
+        if each.lastgroup != None:
+            lexfinal.append(Token(each.lastgroup, each.group()))
+
+    return lexfinal
